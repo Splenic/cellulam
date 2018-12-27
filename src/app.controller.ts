0c82a302fa1c02a1ea7216e34c11c0ea82d21d3b
Fabio Picheli <contato@fabiopicheli.com>
2018-12-27 18:02:16 +0100

    feat: initial streaming example

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Res, Req, Param } from '@nestjs/common';
+import { Request, Response } from 'express';
+import * as fs from 'fs';
+
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +11,38 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('/video/:id')
+  getVideo(@Res() res: Response, @Req() req: Request, @Param('id') id) {
+    const path = `./src/assets/${id}.mp4`;
+    const stat = fs.statSync(path);
+    const fileSize = stat.size;
+    const range = req.headers.range;
+
+    if (range) {
+      const parts = range.replace(/bytes=/, "").split("-");
+      const start = parseInt(parts[0], 10);
+      const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
+      const file = fs.createReadStream(path, {start, end});
+
+      const head = {
+        'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+        'Accept-Ranges': 'bytes',
+        'Content-Length': (end - start) + 1,
+        'Content-Type': 'video/mp4',
+      };
+
+      res.writeHead(206, head);
+      file.pipe(res);
+    } else {
+      const head = {
+        'Content-Length': fileSize,
+        'Content-Type': 'video/mp4',
+      };
+
+      res.writeHead(200, head);
+      fs.createReadStream(path).pipe(res);
+    }
   }
 }
